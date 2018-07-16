5332c012ad8c64898fc4f53bc7486206c930f6ad
YotamShvartsun <yotam.shvartsun@gmail.com>
2018-07-17 00:10:34 +0300

    Fixed bugs

---
+++ consts.py
@@ -0,0 +1,17 @@
+CWORD = "!cht"
+API_URL_BASE = 'http://cht.sh'
+HELP_TEXT = """```bash
+# cheat.sh, the only cheatsheet you need, is now on discord.
+
+# Usage is similar to the cht.sh command line client:
+!cht go reverse a list
+!cht python random list elements
+!cht js parse json
+
+# To display help (this page):
+!cht --help
+
+# For more information, go to:
+# https://github.com/chubin/cheat.sh
+# https://github.com/PaperBag42/cheat.sh-discord
+```"""
