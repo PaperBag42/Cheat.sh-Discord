96385a78467f507d8a4faaf5644bd5e39fe7cc84
PaperBag42 <ben.zichri@gmail.com>
2018-08-30 18:35:32 +0300

    shell mode!

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 
 from logging import log, INFO, WARN
 from sys import argv, exit
-from typing import Dict, List
+from typing import Dict, List, Generator, NewType
 
 from consts import *
 
@@ -15,21 +15,28 @@ TODOs
 1. make a special shell mode for specific channels
 '''
 
+ChannelID = NewType('ChannelID', int)
+
 headers: Dict[str, str] = requests.utils.default_headers()
 headers.update({
 	'User-Agent': 'curl'
 })
 
 class CheatClient(discord.Client):
+	'Costum discord client for cht.sh commands'
+	
+	shells: Dict[ChannelID, str] = {}
+	
+	
 	async def on_server_join(self, server: discord.Server):
 		'''
 		client connected, sends a Hi message to general
 		:return: None
 		'''
-		print(f'Successfully logged in to {server.name}')
+		log(INFO, f'Successfully logged in to {server.name}')
 		for channel in server.channels:
 			if channel.name == 'general':
-				await self.send_message(channel, HELP_TEXT)
+				await self.send_message(channel, HELP_MSG)
 				break
 
 
@@ -39,46 +46,110 @@ class CheatClient(discord.Client):
 		:param message: message received
 		:return: None
 		'''
-		cmd = message.content.split()
+		chnl_id = ChannelID(message.channel.id)
+		cmd = message.content.split('#')[0].split()
+		if not cmd:
+			return
+		
 		if cmd[0] == CWORD:
-			print('{0.author.name} sent: {0.content}'.format(message))
-			if '--help' in cmd or len(cmd) == 1:
-				await self.send_message(message.channel, HELP_TEXT)
+			cmd = cmd[1:]
+			if not cmd or HELP in cmd:  # show help message
+				await self.send_message(message.channel, HELP_MSG)
+			elif cmd[0] == SHELL:  # activate shell mode
+				lang = ''
+				if len(cmd) >= 2:
+					err = check_lang(cmd[1])
+					if err:
+						await self.send_message(message.channel, err)
+						return
+					lang = cmd[1]
+				self.shells[chnl_id] = lang
+				await self.send_message(message.channel, HELP_SHELL.format((' at ' + lang) if lang else ''))
+
 			else:
-				# cht.sh [lang] usually results in an explanation on how to install/compile/run from the terminal
-				lang = LANG_ALIASES.get(cmd[1], cmd[1]) if len(cmd) > 2 else 'bash'
-				max_len = MAX_LEN - len(lang) - 8
-				msg = re.sub(r'\x1b\[.+?m', '', get_cht(cmd))  # simple regex to remove color codes
+				for part in process_request(cmd):
+					await self.send_message(message.channel, part)
 				
-				# splits response to avoid surpassing maximum length
-				while msg != '\n':
-					part = msg[:max_len]
-					last = part.rfind('\n')
-					if last != -1:
-						part = part[:last]
+		elif chnl_id in self.shells:
+			if cmd[0] == 'help':
+				self.send_message(message.channel, HELP_SHELL)
+			elif cmd[0] == 'cd':
+				if len(cmd) < 2 or cmd[1] == '..':
+					self.shells[chnl_id] = ''
+				else:
+					err = check_lang(cmd[1])
+					if err:
+						await self.send_message(message.channel, err)
+						return
+					self.shells[chnl_id] = cmd[1]
+				lang = self.shells[chnl_id]
+				await self.send_message(message.channel, SHELL_CD.format(lang) if lang else SHELL_CD_OUT)
+			elif cmd[0] == 'exit' or cmd[0] == 'quit':
+				del self.shells[chnl_id]
+				await self.send_message(message.channel, SHELL_EXIT)
+			else:
+				for part in process_request([self.shells[chnl_id]] + cmd):
+					await self.send_message(message.channel, part)
 					
-					await self.send_message(message.channel, f'```{lang}\n{part}\n```')
-					msg = msg[len(part):]
+	
+	
+def process_request(cmd: List[str]) -> Generator[str, None, None]:
+	'''
+	Processes request to get a cheat sheet.
+	Splits response to avoid surpassing maximum length.
+	'''
+	res = re.sub(r'\x1b\[.+?m', '', get_cht(cmd))  # simple regex to remove color codes
+	
+	# cht.sh [lang] usually results in an explanation on how to install/compile/run from the terminal
+	lang = LANG_ALIASES.get(cmd[0], cmd[0]) if len(cmd) > 1 else 'bash'
+	max_len = MAX_LEN - len(lang) - 8  # len('```\n\n```')
+				
+	while res != '\n':
+		part = res[:max_len]
+		last = part.rfind('\n')
+		if last != -1:
+			part = part[:last]
+		
+		res = res[len(part):]
+		yield f'```{lang}\n{part}\n```'
 
 
-def get_cht(command: List[str]) -> str:
+
+def get_cht(cmd: List[str]) -> str:
 	'''
-	Gets the output from the cht.sh script.
-	:param command: input for the script
+	Gets the output from the cht.sh server.
+	:param cmd: input for the server
 	'''
-	if '--shell' in command:
+	if '--shell' in cmd:
 		return 'Shell mode is not available.'
 
-	formated_command = command[1] + '/' + '+'.join(command[2:])
-
-	# get a response for command
-	response = requests.get(API_URL_BASE + formated_command, headers=headers)
+	# get a response for cmd
+	response = requests.get(f"{API_URL_BASE}{cmd[0]}/{'+'.join(cmd[1:])}", headers=headers)
 	if response.status_code != 200 and response.status_code != 500:
 		return 'Cannot acsess cheat.sh server at the moment'
 	elif response.status_code == 500:  # internal server error
 		return 'Somthing is wrong with the cheat servers'
 	return response.text
+
+
+def check_lang(lang: str) -> str:
+	'''
+	Finds the language in the language list to make sure it is valid.
+	:return: an empty string if valid, or an error message.
+	'''
+	lines = get_cht([':list']).splitlines()
+	if len(lines) == 1:  # error
+		return lines[0]
 	
+	langs = set()
+	for line in lines:
+		ind = line.find('/')
+		if ind != -1:
+			if lang == line[:ind]:
+				return ''
+			langs.add(line[:ind])
+	return INVALID_LANG.format(lang, '\t'.join(langs))
+
 
 if __name__ == '__main__':
 	logging.basicConfig(level=WARN)
--- consts.py
+++ consts.py
@@ -1,6 +1,10 @@
 CWORD = '!cht'
+HELP = '--help'
+SHELL = '--shell'
+
 API_URL_BASE = 'http://cht.sh/'
-HELP_TEXT = '''```bash
+
+HELP_MSG = '''```bash
 # cheat.sh, the only cheatsheet you need, is now on discord.
 
 # Usage is similar to the cht.sh command line client:
@@ -8,13 +12,35 @@ HELP_TEXT = '''```bash
 !cht python random list elements
 !cht js parse json
 
+# To stop processing message: use '#'
+
 # To display help (this page):
 !cht --help
+
+# To enter shell mode:
+!cht --shell
 
 # For more information, go to:
 # https://github.com/chubin/cheat.sh
 # https://github.com/PaperBag42/cheat.sh-discord
 ```'''
+HELP_SHELL = '''```bash
+# Entering shell mode{}.
+
+help		# show this help
+cd [LANG]	# change/exit the language context
+exit/quit	# exit the cheat shell
+```'''
+SHELL_CD = '`Language changed to {}.`'
+SHELL_CD_OUT = '`Exited language.`'
+SHELL_EXIT = '`Exiting shell mode.`'
+
+INVALID_LANG = '''```
+Invalid section: {}
+Valid sections:
+{}
+```'''
+
 MAX_LEN = 1990
 
 # So what had been bothering me before was caused by the differences between Discord's programming language index (taken from highlight.js)
